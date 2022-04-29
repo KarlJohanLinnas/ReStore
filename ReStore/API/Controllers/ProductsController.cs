0ce0c4e4ce0eda26e187f4e30cd8d730b32507cc
karljohan122 <karljohan122@gmail.com>
2022-04-29 15:30:30 +0300

    Section 2: API Basics

---
+++ ReStore/API/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Configuration;
+using ReStore.API.Data;
+using ReStore.API.Entities;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("API/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+        
+    }
+}
